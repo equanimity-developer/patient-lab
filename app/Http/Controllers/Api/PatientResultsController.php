2bbcfdc37e50ef0e3ac477941e75ae842597f39c
Jacek <bukowski.jacek@gmail.com>
2025-02-21 13:41:57 +0100

    add vue

--- app/Http/Controllers/Api/PatientResultsController.php
+++ app/Http/Controllers/Api/PatientResultsController.php
@@ -7,8 +7,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\OrderResource;
 use App\Http\Resources\PatientResource;
-use App\Models\Order;
-use App\Models\Result;
 use Illuminate\Http\JsonResponse;
 
 class PatientResultsController extends Controller
