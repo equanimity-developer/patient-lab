1aa726e8a8f1cca35b0911f7d0c01871da8744ed
Jacek <bukowski.jacek@gmail.com>
2025-02-23 12:26:47 +0100

    improve axios requests

---
+++ resources/js/utils/auth.js
@@ -0,0 +1,18 @@
+import router from '../router';
+
+export const handleLogout = () => {
+  localStorage.removeItem('token');
+  router.push('/login');
+};
+
+export const getAuthToken = () => {
+  return localStorage.getItem('token');
+};
+
+export const isAuthenticated = () => {
+  return !!getAuthToken();
+};
+
+export const setAuthToken = (token) => {
+  localStorage.setItem('token', token);
+}; 
